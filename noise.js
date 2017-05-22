c86d6c1e4a4320ac04b96d64da56f9631bc866b7
Ting Wong <hellotingwong@gmail.com>
2017-05-22 15:18:47 -0700

    added base keyboard function for click

--- noise.js
+++ noise.js
@@ -1,3 +1,48 @@
- $(document).ready( function() {
-  // your code here
+$(document).ready( function() {
+
+  var playByNote = function(musicNote) {
+    var musicID = "#" + musicNote + "Audio";
+    $(musicID)[0].load();
+    $(musicID)[0].play();
+  };
+
+  $(".instrument").on("click", "button", function(event){
+    var noteClass = $(this).attr("class");
+    noteClass = noteClass.substr(-1);
+    playByNote(noteClass);
+  });
+
+  $(".instrument").on("click", "button", function(event){
+    var noteClass = $(this).attr("class");
+    noteClass = noteClass.substr(-1);
+    playByNote(noteClass);
+  });
 });
+
+
+
+
+// var playByNote = function(musicNote) {
+//   if ($(this).hasClass("c")) {
+//     $("#cAudio")[0].load();
+//     $("#cAudio")[0].play();
+//   } else if ($(this).hasClass("d")) {
+//     $("#dAudio")[0].load();
+//     $("#dAudio")[0].play();
+//   } else if ($(this).hasClass("e")) {
+//     $("#eAudio")[0].load();
+//     $("#eAudio")[0].play();
+//   } else if ($(this).hasClass("f")) {
+//     $("#fAudio")[0].load();
+//     $("#fAudio")[0].play();
+//   } else if ($(this).hasClass("g")) {
+//     $("#gAudio")[0].load();
+//     $("#gAudio")[0].play();
+//   } else if ($(this).hasClass("a")) {
+//     $("#aAudio")[0].load();
+//     $("#aAudio")[0].play();
+//   } else if ($(this).hasClass("b")) {
+//     $("#bAudio")[0].load();
+//     $("#bAudio")[0].play();
+//   }
+// };
